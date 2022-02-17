b32bd22209147c23cae11a0cb12128d11a8ab2c4
anudeeps28 <anudeepsharma28@gmail.com>
2022-02-17 14:51:17 -0500

    basic arduino statements

---
+++ Arduino Programming/if_statement.ino
@@ -0,0 +1,23 @@
+// if statement
+
+// syntax
+if (condition) {
+  //statement(s)
+}
+
+////////////////////////
+
+if (x > 120) digitalWrite(LEDpin, HIGH);
+
+if (x > 120)
+digitalWrite(LEDpin, HIGH);
+
+if (x > 120) {digitalWrite(LEDpin, HIGH);}
+
+if (x > 120) {
+  digitalWrite(LEDpin1, HIGH);
+  digitalWrite(LEDpin2, HIGH);
+}
+// all are correct
+
+////////////////////////
---
+++ Arduino Programming/switch_case.ino
@@ -0,0 +1,30 @@
+// a switch statement compares the value of a variable to the values specified in case statements.
+
+// break used to exit the switch statement
+
+// syntax
+switch (var) {
+  case label1:
+    // statements
+    break;
+  case label2:
+    // statements
+    break;
+  default:
+    // statements
+    break;
+}
+
+// example
+switch (var) {
+  case 1:
+    //do something when var equals 1
+    break;
+  case 2:
+    //do something when var equals 2
+    break;
+  default:
+    // if nothing else matches, do the default
+    // default is optional
+    break;
+}
