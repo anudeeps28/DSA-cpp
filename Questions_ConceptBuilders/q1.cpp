9baad14f0bfce052f5264dd30c75a801f990a064
anudeeps28 <anudeepsharma28@gmail.com>
2022-07-20 16:58:11 -0400

    short circuit

--- Questions_ConceptBuilders/q1.cpp
+++ Questions_ConceptBuilders/q1.cpp
@@ -12,5 +12,9 @@ int main()
 /*
 Q: what is the output of this program
 A: 440
-Concept: 
+Concept: Short Circuiting; also the operations are operated
+from left to right as written in code.
+
+Had the value of c been 0, then the answer would have been
+5,3,-1
 */
