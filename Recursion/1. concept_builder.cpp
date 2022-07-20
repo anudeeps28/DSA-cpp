31252772f40e50e70fb2551889c4f5c0b05c3a3c
anudeeps28 <anudeepsharma28@gmail.com>
2022-07-20 17:00:41 -0400

    head and tail

--- Recursion/1. concept_builder.cpp
+++ Recursion/1. concept_builder.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include<stdio.h>
 
+//tail recursion
 void fun(int n)
 {
     if (n>0)
     {
-        printf("%d\n", n);
+        printf("%d\n", n); // printing during calling time
         fun(n-1); // RECURSION = Function calling itself (with a different parameter)
     }
 }
 
+// head recursion 
+void fun2(int n)
+{
+    if (n>0)
+    {
+        fun2(n-1);
+        printf("%d\n", n); // printing during returning time
+    }
+}
+ 
 int main()
 {
     int x = 3;
     fun(x);
+    fun2(x); 
 }
 
