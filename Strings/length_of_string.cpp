d7b7b8b2c9d27eaf7f71f89397dc9f2773d5539b
anudeeps28 <anudeepsharma28@gmail.com>
2022-08-03 11:43:04 -0400

    strings basics cpp

---
+++ Strings/length_of_string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char s[] = "welcome";
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+
+    }
+
+    cout << "The length of the string is:" << endl << i;
+   return 0;
+}
