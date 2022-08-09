887e1f276261c49437c78a8038e788304de8780a
anudeeps28 <anudeepsharma28@gmail.com>
2022-08-09 19:17:57 -0400

    Linked Lists

---
+++ Linked List/displaying.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*first; // declaring a global pointer here only
+
+// function to create a linked list from an array
+void create(int A[], int n)
+{
+    int i;
+    struct  Node *t, *last;
+    first = new Node;
+    first -> data = A[0];
+    first -> next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t ->data = A[i];
+        t->next = NULL;
+        last ->next = t;
+        last = t;
+        
+    }
+}
+
+
+// function to display the linked list
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data;
+        p = p ->next;
+    }
+
+}
+
+int main()
+{
+
+    int A[] = {3,5,7,10,15};
+
+    create(A,5);
+
+    Display(first);
+
+    return 0;
+}
