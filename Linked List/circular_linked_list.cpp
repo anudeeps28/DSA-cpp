6fcde55b601fb91d00fd133b2de33bfde63c8ad0
anudeeps28 <anudeepsharma28@gmail.com>
2022-08-12 17:21:13 -0400

    change

--- Linked List/circular_linked_list.cpp
+++ Linked List/circular_linked_list.cpp
@@ -1,1 +1,50 @@
- 
+#include<iostream>
+using namespace std;
+
+// structure for circular linked list
+struct Node
+{
+    int data;
+    struct Node *next;
+}*Head;
+
+// creating the circular linked list
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    Head = new Node;
+    Head ->data = A[0];
+    Head->next = Head;
+
+    // creating new node
+    for (i = 0; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        last->next =  t;
+        last = t;
+    }
+
+}
+
+void Display(struct Node *h)
+{
+    // we cannot use while because it is not possible (if we give the condition, the first condition will itself be false)
+    do
+    {
+        cout << h->data;
+        h = h->next;
+    } while (h != Head);
+    
+        
+}
+
+int main()
+{   
+    int A[] = {2,3,4,5,6};
+    create(A,5);
+    Display(Head);
+    return 0;
+}
