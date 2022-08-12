bf18dcc46f4f7a49be2b25a17e0ea09c77b006a2
anudeeps28 <anudeepsharma28@gmail.com>
2022-08-12 17:23:19 -0400

    insert in ll

---
+++ Linked List/insert_circularLinkedList.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+using namespace std;
+
+// structure for circular linked list
+struct Node
+{
+    int data;
+    struct Node *next;
+}*Head;
+
+// creating the circular linked list
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    Head = new Node;
+    Head ->data = A[0];
+    Head->next = Head;
+
+    // creating new node
+    for (i = 0; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        last->next =  t;
+        last = t;
+    }
+
+}
+
+void Display(struct Node *h)
+{
+    // we cannot use while because it is not possible (if we give the condition, the first condition will itself be false)
+    do
+    {
+        cout << h->data;
+        h = h->next;
+    } while (h != Head);
+    
+        
+}
+
+// function to find the length of a circular linked list
+
+/*
+This function is the same that we used in linear linked list,
+just that here we needed to use the do while loop
+*/
+
+int Length(struct Node *p)
+{
+    int len = 0;
+    do
+    {
+        len++;
+        p = p->next;
+    } while (p != Head);
+    
+    return len;
+}
+
+void insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+  
+    // checking if the index is valid or not
+    if (index < 0 || index > Length(p))
+        return;
+
+    // Inserting before head if the index is 0
+    if (index == 0)
+    {
+        t = new Node;
+        t->data = x;
+        if (Head == NULL)
+        {
+            Head = t;
+            Head->next= Head;
+        }
+        // if there are some nodes present already in the linked list
+        else
+        {
+            while (p->next != Head)
+                p = p->next;
+
+            // after p has reached the last node
+            p->next = t;
+            t->next = Head;
+            Head = t;
+        }
+    }
+
+    // else if the index is not 0, the code is same as that of inserting in a linear linked list
+    else
+    {
+        // traversing to the node where we need to insert
+        for (i = 0; i < index -1; i++)
+            p = p->next;
+
+        // creating new node
+        t = new Node;
+        t->data = x;
+        t->next = p->next;
+        p->next = t;
+
+    } 
+}
+
+int main()
+{   
+    int A[] = {2,3,4,5,6};
+    create(A,5);
+    Display(Head);
+    return 0;
+}
---
+++ Linked List/insert_doublyLinkedList.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next; 
+}*first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    int i;
+
+    first = new Node;
+    first->data = A[0];
+    first->prev = first->next = NULL;
+    last = first;
+
+    // forming the linked list
+    for (i = 1;i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p)
+    {
+        cout << p->data;
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int Length(struct Node *p)
+{
+    int len = 0;
+
+    while (p)
+    {
+        len ++;
+        p = p->next;
+
+    }
+
+    return len;
+}
+
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+
+    // checking if the index is valid or not
+    if ((index < 0) || index > Length(p))
+        return;
+    
+    // if index = 0, then it is a special case (we have to insert a node before the first node)
+    if (index == 0)
+    {
+        t = new Node;
+        t->data = x;
+        t->prev =NULL;
+        t->next = first;
+        first ->prev = t;
+        first = t;
+    }
+
+    else // inserting at any other position
+    {
+        for (i = 0; i < index-1; i++)
+        {
+            p = p->next;
+        }
+        
+        t = new Node;
+        t->data = x;
+        t->prev = p;
+        t->next = p->next;
+        if (p->next)
+            p->next->prev = t;
+        p->next = t;
+    }
+
+}
+
+int main()
+{
+    int A[] = {1,2,3,4,45};
+    create(A,5);
+    
+    cout << "length is" << Length(first);
+
+    Display(first);
+
+    Insert(first,5,25);
+
+    Display(first);
+    return 0;
+}
