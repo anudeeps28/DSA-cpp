2c65a262e18c5f496851da175b9828da54495c75
anudeeps28 <anudeepsharma28@gmail.com>
2022-08-18 19:25:08 -0400

    delete and print remaining

--- Queue/Queue_ADT.cpp
+++ Queue/Queue_ADT.cpp
@@ -19,24 +19,62 @@ void Create(struct Queue *q, int size)
 }
 
 // adding elements to a queue = enqueue
-void enqueue(struct Queue *q,int x)
+void enqueue(struct Queue *q, int x)
 {
-    if(q->rear == q->size - 1)
+    if(q->rear == q->size - 1) // checking if the queue is already full
         cout << "queue is full";
     else
     {
         q->rear++;
-        Q[q->rear] = x;
+        q->Q[q->rear] = x;
     }
 }
 
+// dequeue = deleting elements from the queue
+int dequeue(struct Queue *q)
+{
+    int x = -1;
 
+    if(q->rear == q->front) // checking if the queue is empty already or not
+        cout << "queue is empty";
+    else
+    {
+        q->front++;
+        x = q->Q[q->front]; // value in array wherever front is pointing
+    }
+    return x;
+} 
+
+// display function
+void Display(struct Queue q) // we don't neet to change the elements, so we call by value only
+{
+    int i;
+
+    for (i = q.front+1;i<=q.rear;i++)
+        cout << q.Q[i];
+    cout << endl;
+}
 
 int main()
 {
     // creating object inside main function
-    struct Queue q;
+    struct Queue q; // for accessing stuff
     Create(&q,5);
+
+    // inserting elements
+    enqueue(&q,10);
+    enqueue(&q,20);
+    enqueue(&q,30);
+    enqueue(&q,40);
+
+    // displaying elements
+    Display(q);
+
+    // deleting first element
+    cout << dequeue(&q) << endl;
+
+    // // displaying after deleting
+    Display(q);
 
     return 0;
 }
