5116edf2132bb1340500c10062738e6667405e86
anudeeps28 <anudeepsharma28@gmail.com>
2022-08-18 19:08:46 -0400

    structure

--- Queue/Queue_ADT.cpp
+++ Queue/Queue_ADT.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 using namespace std;
 
+// structure of queue
+struct Queue
+{
+    int size;
+    int front; // front pointer
+    int rear; // rear pointer
+    int *Q; // space for storing elements (dynamically creating memory in heap)
+};
+
+// creating queue
+void Create(struct Queue *q, int size)
+{
+    q->size = size;
+    q->front = q->rear = -1;
+    q->Q = new int[size];
+}
+
+// adding elements to a queue = enqueue
+void enqueue(struct Queue *q,int x)
+{
+    if(q->rear == q->size - 1)
+        cout << "queue is full";
+    else
+    {
+        q->rear++;
+        Q[q->rear] = x;
+    }
+}
+
+
+
 int main()
 {
+    // creating object inside main function
+    struct Queue q;
+    Create(&q,5);
 
     return 0;
 }
