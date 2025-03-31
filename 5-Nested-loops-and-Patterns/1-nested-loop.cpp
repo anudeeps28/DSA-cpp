d806053617b613d6ad2d10aff78085b07bfbdf5d
anudeeps28 <anudeepsharma28@gmail.com>
2025-03-30 23:15:33 -0400

    nested loops

---
+++ 5-Nested-loops-and-Patterns/1-nested-loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+/**
+ * 1 - outer loop: number of rows
+ * 2 -  inner loop: number of columns (how many items are in each row)
+ * 3 - code inside inner loop: what will each row be
+ */
+int main() {
+    int n = 9; // you can change this to print different patterns
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
