41a8107b8b5de86689937eda5525d895cf669533
anudeeps28 <anudeepsharma28@gmail.com>
2025-04-01 15:03:36 -0400

    patterns lecture problems

--- 5-Nested-loops-and-Patterns/5-character-pyramid-pattern.cpp
+++ 5-Nested-loops-and-Patterns/5-character-pyramid-pattern.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main() {
-    int n=10;
+    int n = 4;
+    char letter = 'A';
     for(int i=1; i<=n; i++) {
-        for(int j=(n-i+1); j>=1; j--) {
-            cout << "* "; 
+        for(int j=1; j<=i; j++) {
+            cout << letter++;
         }
         cout << endl;
     }
