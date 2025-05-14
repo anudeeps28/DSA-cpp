c251d7c01dfef16ba2732e23ad67c64b237ab4a8
anudeeps28 <anudeepsharma28@gmail.com>
2025-05-14 15:15:33 -0400

    min --> minIdx for better readibility

--- 10-Basic-Sorting-Algorithms/3-insertion-sort.cpp
+++ 10-Basic-Sorting-Algorithms/3-insertion-sort.cpp
@@ -13,12 +13,12 @@ void insertionSort(int arr[], int n) {
     // Loop through each element starting from index 0 to n-1
     for(int i=0; i<n; i++) {
         int curr = arr[i]; // Store the current element that needs to be placed correctly
-        int prev = i-1;    // Start comparing from the previous element
+        int prevIdx = i-1;    // Start comparing from the previous element
 
         // Move elements of the sorted segment forward if they are greater than curr
-        while (prev >=0 && arr[prev] > curr) {
-            swap(arr[prev], arr[prev+1]); // Swap the previous element with the current to shift larger elements forward
-            prev--; // Move to the next previous element for comparison
+        while (prevIdx >=0 && arr[prevIdx] > curr) {
+            swap(arr[prevIdx], arr[prevIdx+1]); // Swap the previous element with the current to shift larger elements forward
+            prevIdx--; // Move to the next previous element for comparison
         }
     }
 }
