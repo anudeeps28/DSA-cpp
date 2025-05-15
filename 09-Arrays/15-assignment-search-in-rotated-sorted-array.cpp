7610a00bbbc8ade6e391f100c9aa1ad241840ca9
anudeeps28 <anudeepsharma28@gmail.com>
2025-05-15 19:04:04 -0400

    brute force + leetcode link

--- 09-Arrays/15-assignment-search-in-rotated-sorted-array.cpp
+++ 09-Arrays/15-assignment-search-in-rotated-sorted-array.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 
+/**
+ * @brief LeetCode Question:
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ */
+
 int search(int nums[], int n, int target) {
     int low = 0, high = n - 1;
     while (low <= high) {
@@ -30,11 +35,22 @@ int search(int nums[], int n, int target) {
     return -1;
 }
 
+// O(n) 
+int bruteForceSearch(int arr[], int n, int target) {
+    for(int i=0; i<n; i++) {
+        if(arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
     int nums[] = {4, 5, 6, 7, 0, 1, 2};
     int n = sizeof(nums) / sizeof(int);
 
     cout << search(nums, n, 0) << "\n";  // prints 4
     cout << search(nums, n, 3) << "\n";  // prints -1
+    cout << bruteForceSearch(nums, n, 0); // prints 4
     return 0;
 }
