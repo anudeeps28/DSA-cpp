b159e8c9c7af5ee256942cc8b7bb87d8e7d202e2
anudeeps28 <anudeepsharma28@gmail.com>
2025-05-15 19:04:42 -0400

    kadane's like algo - optimized soln

--- 09-Arrays/15-assignment-maximum-product-subarray.cpp
+++ 09-Arrays/15-assignment-maximum-product-subarray.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief LeetCode Question:
+ * https://leetcode.com/problems/maximum-product-subarray/description/ 
+ */
+
+//  O(n)
+void optimizedSoln(int arr[], int n) {
+    if (n == 0) {
+        cout << 0 << endl;
+        return;
+    }
+    int maxProd = arr[0];
+    int minProd = arr[0];
+    int result = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < 0) {
+            swap(maxProd, minProd);
+        }
+        maxProd = max(arr[i], maxProd * arr[i]);
+        minProd = min(arr[i], minProd * arr[i]);
+        result = max(result, maxProd);
+    }
+    cout << result << endl;
+}
+
 void maxProdSubArray(int array[], int n) {
    int maxProd = INT_MIN;
    for (int start = 0; start < n; start++) {
@@ -12,13 +37,14 @@ void maxProdSubArray(int array[], int n) {
          maxProd = max(maxProd, currProd);
       }
    }
-   cout << maxProd;
+   cout << maxProd << endl;
 }
 
 int main() {
-   int nums[] = {-2,0,-1};
+   int nums[] = {2,3,-2,4};
    int n = sizeof(nums) / sizeof(nums[0]);
     
    maxProdSubArray(nums, n);
+   optimizedSoln(nums, n);
    return 0;
 }
